52fea91c2d439bac9d1f4687b8222f6f686b2a8a
Kevin <gogamekevin123@gmail.com>
2025-06-16 17:48:30 +0800

    Update folder cleanup script

--- simulation/Gslice/data/clean_data.sh
+++ simulation/Gslice/data/clean_data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir -p ./miss_ddl
+mkdir -p ./train_time
+mkdir -p ./used_gpus
+mkdir -p ./used_gpus_infer
+mkdir -p ./used_gpus_train
+mkdir -p ./dist
+mkdir -p ./avg_lat
+
 rm -f ./miss_ddl/*
 rm -f ./train_time/*
 rm -f ./used_gpus/*
